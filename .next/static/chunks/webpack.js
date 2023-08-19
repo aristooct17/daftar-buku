9d5ad00babcab615d7dde5df3b260681d03d9053
aristo_oct <aristo.oct@gmail.com>
2023-08-20 01:46:22 +0700

    Fixing dependencies

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f89e7061d4125492"; }
+/******/ 		__webpack_require__.h = function() { return "250e7d77693aec34"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
