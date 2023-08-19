d49e538b2826bacfba9e47b83111eeff61e70bc7
aristooct17 <aristo.oct@gmail.com>
2023-08-20 00:49:02 +0700

    Add files via upload

---
+++ src/pages/types/detailBukuList.ts
@@ -0,0 +1,19 @@
+export type DetailBukuLists = {
+    id: number;
+    image: string;
+    bookTitle: string;
+    writer: string;
+    year: string;
+};  
+
+export type DetailBukuType = {
+    name: string;
+    username: string;
+    email: string;
+    phone: string;
+    website: string;
+};
+
+export type DetailBukuProps = {
+    id: number;
+}
